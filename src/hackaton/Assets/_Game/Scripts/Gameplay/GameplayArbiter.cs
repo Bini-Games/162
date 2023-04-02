c5a715e8a2e3103d059bbd9e20844094a603500d
Volodymyr Nagibator228 Pasichko <volodymyr.pasichko@bini.games>
2023-04-02 23:45:17 +0300

    final fix

--- src/hackaton/Assets/_Game/Scripts/Gameplay/GameplayArbiter.cs
+++ src/hackaton/Assets/_Game/Scripts/Gameplay/GameplayArbiter.cs
@@ -8,18 +8,22 @@ namespace Gameplay
     public class GameplayArbiter : IInitializable
     {
         private LevelBuilder levelBuilder;
+        private GameplayScore score;
 
         [Inject]
-        public GameplayArbiter(LevelBuilder levelBuilder) => 
+        public GameplayArbiter(LevelBuilder levelBuilder, GameplayScore score)
+        {
             this.levelBuilder = levelBuilder;
+            this.score = score;
+        }
 
         public void Initialize() => 
             levelBuilder.OnObjectsFinished += (_, __) => FinishGame();
 
         private async void FinishGame()
         {
-            await UniTask.Delay(TimeSpan.FromSeconds(3));
-            SceneManager.LoadScene("FinishGame");
+            await UniTask.Delay(TimeSpan.FromSeconds(2));
+            SceneManager.LoadScene(score.Value > 0 ? "FinishGameGood" : "FinishGameBad");
         }
     }
 }
