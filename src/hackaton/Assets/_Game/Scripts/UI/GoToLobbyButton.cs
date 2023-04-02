8a4a0dc62b4a395c3df492b4e02ad84ee0ab2abd
Volodymyr Nagibator228 Pasichko <volodymyr.pasichko@bini.games>
2023-04-02 21:32:09 +0300

    Script finish page

---
+++ src/hackaton/Assets/_Game/Scripts/UI/GoToLobbyButton.cs
@@ -0,0 +1,10 @@
+﻿namespace _Game.Scripts.UI
+{
+    public class GoToLobbyButton : UIButton
+    {
+        private GameStateMachine gameStateMachine;
+        
+        protected override void OnClick() => 
+            gameStateMachine.Enter<EnteringToMainMenuState>();
+    }
+}
