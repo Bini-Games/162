3aa5a2f2e1a6ed9c10d1bb7321f2b66e0912df33
Volodymyr Nagibator228 Pasichko <volodymyr.pasichko@bini.games>
2023-04-02 21:35:34 +0300

    Fix go to lobby button

--- src/hackaton/Assets/_Game/Scripts/UI/GoToLobbyButton.cs
+++ src/hackaton/Assets/_Game/Scripts/UI/GoToLobbyButton.cs
@@ -1,8 +1,16 @@
-﻿namespace _Game.Scripts.UI
+﻿using Zenject;
+
+namespace _Game.Scripts.UI
 {
     public class GoToLobbyButton : UIButton
     {
         private GameStateMachine gameStateMachine;
+
+        [Inject]
+        private void Construct(GameStateMachine gameStateMachine)
+        {
+            this.gameStateMachine = gameStateMachine;
+        }
         
         protected override void OnClick() => 
             gameStateMachine.Enter<EnteringToMainMenuState>();
